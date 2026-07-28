760ae26ba587ed39503d7dd4b988410983e35f82
sujalmitra23 <sujal.kumar.mitra@gmail.com>
2026-07-28 12:52:26 +0530

    Added KMS for tfstate bucket

--- Terraform_Sujal/TF_STATE_S3_BUCKET/main.tf
+++ Terraform_Sujal/TF_STATE_S3_BUCKET/main.tf
@@ -1,14 +1,91 @@
-#resource "random_string" "suffix" {
-#  length  = 6
-#  upper   = false
-#  special = false
-#}
+data "aws_iam_policy_document" "kms_policy" {
+
+  statement {
+    sid    = "EnableManagementAccountAdministration"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::048408301799:root"]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowDevAccountUsage"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::802589443968:root"]
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:GenerateDataKey",
+      "kms:DescribeKey",
+      "kms:ReEncrypt*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowProdTerraformRoleUsage"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::133089468258:role/TerraformDeploymentRole"
+      ]
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:Encrypt",
+      "kms:GenerateDataKey",
+      "kms:DescribeKey",
+      "kms:ReEncrypt*"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+###################################################
+# KMS KEY
+###################################################
+
+resource "aws_kms_key" "terraform_state" {
+  description             = "KMS Key for Terraform State Bucket"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+  policy = data.aws_iam_policy_document.kms_policy.json
+
+  tags = {
+    Name        = "terraform-state-kms"
+    Environment = "management"
+  }
+}
+
+resource "aws_kms_alias" "terraform_state" {
+  name          = "alias/terraform-state"
+  target_key_id = aws_kms_key.terraform_state.key_id
+}
+
+###################################################
+# S3 STATE BUCKET
+###################################################
 
 resource "aws_s3_bucket" "tftstate_bucket" {
   #bucket = "tfstate-${var.environment_name}-${var.aws_region}-${random_string.suffix.result}"
   bucket = "tfstate-bucket-sujal-mitra"
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 
   tags = {
@@ -17,6 +94,22 @@ resource "aws_s3_bucket" "tftstate_bucket" {
     Environment = "All"
   }
 }
+
+###################################################
+# OWNERSHIP CONTROLS
+###################################################
+
+resource "aws_s3_bucket_ownership_controls" "tfstate" {
+  bucket = aws_s3_bucket.tftstate_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+###################################################
+# PUBLIC ACCESS BLOCK
+###################################################
 
 resource "aws_s3_bucket_public_access_block" "tftstate_bucket_publicaccess" {
   bucket = aws_s3_bucket.tftstate_bucket.id
@@ -27,6 +120,10 @@ resource "aws_s3_bucket_public_access_block" "tftstate_bucket_publicaccess" {
   restrict_public_buckets = true
 }
 
+###################################################
+# VERSIONING
+###################################################
+
 resource "aws_s3_bucket_versioning" "tftstate_bucket_versioning" {
   bucket = aws_s3_bucket.tftstate_bucket.id
   versioning_configuration {
@@ -34,12 +131,46 @@ resource "aws_s3_bucket_versioning" "tftstate_bucket_versioning" {
   }
 }
 
+# resource "aws_s3_bucket_server_side_encryption_configuration" "tftstate_bucket_encryption" {
+#   bucket = aws_s3_bucket.tftstate_bucket.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
+###################################################
+# SERVER SIDE ENCRYPTION
+###################################################
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "tftstate_bucket_encryption" {
   bucket = aws_s3_bucket.tftstate_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.terraform_state.arn
+      sse_algorithm     = "aws:kms"
+    }
+
+    bucket_key_enabled = true
+  }
+}
+
+###################################################
+# LIFECYCLE CONFIGURATION
+###################################################
+
+resource "aws_s3_bucket_lifecycle_configuration" "tfstate_lifecycle" {
+  bucket = aws_s3_bucket.tftstate_bucket.id
+
+  rule {
+    id     = "cleanup-old-versions"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 90
     }
   }
 }
